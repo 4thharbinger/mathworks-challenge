1154295f8d88c959ec72a8512b2f766d3a635c9e
mjg1107 <michaelgomezjared@gmail.com>
2026-07-15 22:47:12 -0700

    Add files via upload

---
+++ RUN_ME.m
@@ -0,0 +1,10 @@
+% RUN_ME.m
+%
+% HOW TO USE (no typing required):
+%   1. Double-click this file to open it in MATLAB's Editor.
+%   2. Click the green "Run" button at the top of the Editor tab
+%      (or press F5).
+
+
+cd(fileparts(mfilename('fullpath')));
+FluxionAeroApp;
